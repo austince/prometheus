b3770e010a5d1d8e85a152d6656db4fbc4ca74de
austin ce <austin.cawley@gmail.com>
2021-06-21 13:37:23 -0400

    Use buf to build and vitess protoc plugin

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -10,8 +10,8 @@ if ! [[ "$0" =~ scripts/genproto.sh ]]; then
 	exit 255
 fi
 
-if ! [[ $(protoc --version) =~ 3.15.8 ]]; then
-	echo "could not find protoc 3.15.8, is it installed + in PATH?"
+if ! [[ $(buf --version) =~ 0.43.2 ]]; then
+	echo "could not find buf 0.43.2, is it installed + in PATH?"
 	exit 255
 fi
 
@@ -20,7 +20,6 @@ fi
 cp go.sum go.sum.bak
 
 INSTALL_PKGS=(
-  "golang.org/x/tools/cmd/goimports"
   "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
   "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 )
@@ -32,43 +31,13 @@ done
 GET_PKGS=(
   "google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0"
   "google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1.0"
+  "github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@v0.0.0-20210616093554-9236f7c7b8ca"
 )
 for pkg in "${GET_PKGS[@]}"; do
     echo "getting $pkg"
     GO111MODULE=on go get "$pkg"
 done
 
-
-MAPPINGS=(
-  "google/protobuf/descriptor.proto=github.com/golang/protobuf/protoc-gen-go/descriptor"
-)
-MAPPING_ARG=""
-for mapping in "${MAPPINGS[@]}"
-do
-  MAPPING_ARG+="M$mapping"
-done
-
-PROM_ROOT="${PWD}"
-PROM_PATH="${PROM_ROOT}/prompb"
-GRPC_GATEWAY_ROOT="$(GO111MODULE=on go list -mod=readonly -f '{{ .Dir }}' -m github.com/grpc-ecosystem/grpc-gateway)"
-
-DIRS="prompb"
-
-echo "generating code"
-for dir in ${DIRS}; do
-	pushd ${dir}
-		protoc \
-		        --go_out="$MAPPING_ARG":. \
-		        --go_opt=paths=source_relative \
-		        --go-grpc_out="$MAPPING_ARG":. \
-		        --go-grpc_opt=paths=source_relative \
-		        -I=. \
-            -I="${PROM_PATH}" \
-            -I="${GRPC_GATEWAY_ROOT}/third_party/googleapis" \
-            ./*.proto
-
-		goimports -w ./*.go
-	popd
-done
+buf generate --verbose
 
 mv go.sum.bak go.sum
